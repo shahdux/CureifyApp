a4da41ca4c08a61cbd43e81d491eb8cf85843794
shahdMohammad <shahdmohammadx@gmail.com>
2026-04-13 21:20:27 +0200

    first commit

---
+++ cureifyapp/src/RoutingApp.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+
+const RoutingApp = () => {
+    return ( 
+        <BrowserRouter>
+        <Routes>
+  {/* <Route path='/' element={<Home />} /> */}
+   
+
+
+
+
+        </Routes>
+        
+    
+        </BrowserRouter>
+     );
+}
+ 
+export default RoutingApp;
