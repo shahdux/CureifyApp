5f666e32b3133af6ee8010e2beacd52cd77ddf31
shahdMohammad <shahdmohammadx@gmail.com>
2026-04-19 20:22:27 +0200

    activate checklist in home page

--- cureifyapp/src/components/MedicineCard.jsx
+++ cureifyapp/src/components/MedicineCard.jsx
@@ -1,24 +1,27 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import "./MedicineCard.css"
 
 const MedicineCard = (props) => {
+    const [taken, setTaken] = useState(false);
+
     return (
         <div className='medcard' style={{ width: props.width1 }}>
             <div className='forfirst2' style={{ width: props.width }}>
-
                 <img src={props.image} alt="medication icon" />
-
-            <div className='medcardInfo'>
-                <p className='medcardName'>{props.name}</p>
-                <div className='medcardMeta'>
-                    <span className='medcardTime'>{props.time}</span>
-                    <span className='medcardDot' />
-                    <span className='medcardTime'>{props.instruction}</span>
+                <div className='medcardInfo'>
+                    <p className='medcardName'>{props.name}</p>
+                    <div className='medcardMeta'>
+                        <span className='medcardTime'>{props.time}</span>
+                        <span className='medcardDot' />
+                        <span className='medcardTime'>{props.instruction}</span>
+                    </div>
                 </div>
             </div>
-            </div>
 
-            <button className='medcardToggle' />
+            <button
+                className={`medcardToggle ${taken ? 'medcardToggleTaken' : ''}`}
+                onClick={() => setTaken(t => !t)}
+            />
         </div>
     );
 }
